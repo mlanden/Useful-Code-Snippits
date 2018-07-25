1af0054155dfc3ef7cca70a78b3a169969bc3e24
Matthew Landen <mlanden@gatech.edu>
2018-07-25 15:32:41 -0400

    cross validation

--- classification/binary_classification.py
+++ classification/binary_classification.py
@@ -3,6 +3,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.datasets import imdb
 import numpy as np
+from evaluate import cross_validation
 
 def vectorize_data(data, dimension = 10000):
     vectors = np.zeros((len(data), dimension))
@@ -30,6 +31,17 @@ def graph_train_history(history):
     plt.legend()
     plt.show()
 
+def build_model():
+    model = Sequential()
+    model.add(Dense(16, activation = 'relu', input_shape = (10000,)))
+    model.add(Dense(16, activation = 'relu'))
+    model.add(Dense(1, activation = 'sigmoid'))
+
+    model.compile(optimizer = 'adam', #'rmsprop',
+                loss = 'binary_crossentropy',
+                metrics = ['accuracy'])
+    return model
+
 (train_data, train_labels), (test_deta, test_labels) = \
     imdb.load_data(num_words = 10000)
 
@@ -37,26 +49,27 @@ x_train = vectorize_data(train_data)
 y_train = np.asarray(train_labels).astype('float32')
 x_test = vectorize_data(test_deta)
 y_test = np.asarray(test_labels).astype('float32')
-x_val = x_train[:10000]
-partial_x_train = x_train[10000:]
-y_val = y_train[:10000]
-partial_y_train = y_train[10000:]
 
-model = Sequential()
-model.add(Dense(16, activation = 'relu', input_shape = (10000,)))
-model.add(Dense(16, activation = 'relu'))
-model.add(Dense(1, activation = 'sigmoid'))
+histories, results = cross_validation((x_train, y_train), (x_test, y_test), n_folds = 5, 
+    build_model = build_model,  num_validation_samples = 10000, epochs = 10,
+    batch_size = 512)
 
-model.compile(optimizer = 'adam', #'rmsprop',
-            loss = 'binary_crossentropy',
-            metrics = ['accuracy'])
-history = model.fit(partial_x_train,
-                    partial_y_train,
-                    epochs = 10,
-                    batch_size = 512,
-                    validation_data = (x_val, y_val))
-
-results = model.evaluate(x_test, y_test)
+print(histories)
 print()
 print(results)
-graph_train_history(history)
+# x_val = x_train[:10000]
+# partial_x_train = x_train[10000:]
+# y_val = y_train[:10000]
+# partial_y_train = y_train[10000:]
+
+
+# history = model.fit(partial_x_train,
+#                     partial_y_train,
+#                     epochs = 10,
+#                     batch_size = 512,
+#                     validation_data = (x_val, y_val))
+
+# results = model.evaluate(x_test, y_test)
+# print()
+# print(results)
+# graph_train_history(history)
---
+++ classification/evaluate.py
@@ -0,0 +1,71 @@
+
+def split_data(train_data, num_validation_samples):
+    eval_x, eval_y = train_data
+    val_x = eval_x[:num_validation_samples]
+    val_y = eval_y[:num_validation_samples]
+    train_x = eval_x[num_validation_samples:]
+    train_y = eval_y[num_validation_samples:]
+    return train_x, train_y, val_x, val_y
+
+
+def held_out_evaluation(train_data, test_data, build_model, 
+    num_validation_samples, epochs, batch_size):
+    train_x, train_y, val_x, val_y = split_data(train_data, num_validation_samples)
+
+    model = build_model()
+    history = model.fit(train_x, 
+                        train_y,
+                        epochs = epochs,
+                        batch_size = batch_size,
+                        validation_data = (val_x, val_y))
+    val_info = history.history
+
+    # here id where you tune hyper parameters,
+    # Only test model below right before it's ready for production systems
+    test_x, test_y = test_data
+    test_model = build_model()
+    results = model.evaluate(test_x, test_y)
+    print(results)
+
+def cross_validation(train_data, test_data, n_folds, build_model, 
+    num_validation_samples, epochs, batch_size):
+    train_x, train_y = train_data
+    test_x, test_y = test_data
+
+    histories = []
+    from sklearn.model_selection import StratifiedKFold 
+    skf = StratifiedKFold(n_splits = n_folds)
+    fold = 0
+    for train_idx, val_idx in skf.split(train_x, train_y):
+        fold += 1
+        print("Beginning fold", fold)
+        train_x_fold = train_x[train_idx]
+        train_y_fold = train_y[train_idx]
+        val_x = train_x[val_idx]
+        val_y = train_y[val_idx]
+
+        model = build_model()
+        history = model.fit(train_x_fold,
+                  train_y_fold,
+                  epochs = epochs,
+                  batch_size = batch_size,
+                  validation_data = (val_x, val_y))
+        histories.append(history)
+
+    model = build_model()
+    model.fit(train_x,
+              train_y,
+              epochs = epochs,
+              batch_size = batch_size)
+    results = model.evaluate(test_x, test_y)
+    return histories, results
+
+
+
+
+
+
+
+
+
+
